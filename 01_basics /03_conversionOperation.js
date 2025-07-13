7796afb2d4acdfd13c1d83604d106228417dcbb9
Sagar17082000 <61305869+Sagar17082000@users.noreply.github.com>
2025-07-13 11:25:07 +0000

    Comparison

--- 01_basics /03_conversionOperation.js
+++ 01_basics /03_conversionOperation.js
@@ -27,8 +27,41 @@ let someNumber = 33
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // "33" => 33
+
+/*********************** Operation*************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Sagar"
+let str3 = str1 + str2
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+
+// console.log(1+2+"2");
+
+// console.log(+true)
+
+// console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
 
