2bc08595910a5ff169334aeb26f291fa7179bf2e
Sagar17082000 <61305869+Sagar17082000@users.noreply.github.com>
2025-07-13 10:52:11 +0000

    Conversion Operation

---
+++ 01_basics /01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345;
+
+let accountEmail = "sr@sr.com";
+var accountPassword = "12345";
+accountCity = "Nagpur"
+let accountState;
+
+
+console.log(accountId);
+
+/*
+Prefer not to use var keyword
+because of issues related to block scope & functional scope 
+*/
+
+console.table([accountId , accountEmail,accountPassword,accountCity,accountState]);
