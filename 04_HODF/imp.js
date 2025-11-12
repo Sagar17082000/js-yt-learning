13df1ddbcffe7909eb7ae882c913a0c55fd5e073
Sagar17082000 <61305869+Sagar17082000@users.noreply.github.com>
2025-11-12 16:13:47 +0000

    DOM Manipulation

---
+++ 04_HODF/imp.js
@@ -0,0 +1,15 @@
+// title.textContent
+/*
+
+    <h1 id="title" class="heading">DOM Learning on chai and code
+        <span style="display:none">test text</span>
+    </h1>
+
+title.innerText
+'DOM Learning on chai and code'
+title.textContent
+'DOM Learning on chai and code\n        test text\n    '
+title.innerHTML
+'DOM Learning on chai and code\n        <span style="display:none">test text</span>\n  
+
+*/
